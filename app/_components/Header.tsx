5cfa2307801cf24cb616dd72b770cc4abe49f081
isanchez-userfront <147533431+isanchez-userfront@users.noreply.github.com>
2024-08-16 01:26:04 -0500

    Header, dashboard and auth layout style (#4)
    Fix the size of auth forms, the spacing of header and dashboard. Wrap JWT in code styled block.

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -7,27 +7,27 @@ export default function Header() {
   const { isAuthenticated, logout } = useUserfront();
 
   return (
-    <header className="p-4">
+    <header>
       <nav className="flex justify-between items-center">
         <Link href="/" className="text-lg font-bold">
           Home
         </Link>
-        <div className="flex gap-4">
+        <div className="flex items-center gap-4 md:gap-8">
           {isAuthenticated ? (
             <>
               <Link href="/dashboard">Dashboard</Link>
               <button
                 onClick={() => logout({ redirect: "/login" })}
-                className="bg-blue-500 rounded"
+                className="px-2 py-1 bg-[#F2055C] font-medium rounded"
               >
                 Logout
               </button>
             </>
           ) : (
             <>
-              <Link href='/login'>Login</Link>
-              <Link href='/signup'>Signup</Link>
-              <Link href='/reset'>Reset Password</Link>
+              <Link href="/login">Login</Link>
+              <Link href="/signup">Signup</Link>
+              <Link href="/reset">Reset Password</Link>
             </>
           )}
         </div>
