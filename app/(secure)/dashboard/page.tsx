b5af70bd2c449f3b77ca05cebae7ff7caec1ebc1
isanchez-userfront <147533431+isanchez-userfront@users.noreply.github.com>
2024-08-16 01:24:15 -0500

    Header, dashboard and auth layout style
    Fix the size of auth forms, the spacing of header and dashboard. Wrap JWT in code styled block.

--- app/(secure)/dashboard/page.tsx
+++ app/(secure)/dashboard/page.tsx
@@ -1,20 +1,30 @@
 "use client";
 
-import * as React from 'react';
-import { useUserfront } from '@userfront/next/client';
+import * as React from "react";
+import { useUserfront } from "@userfront/next/client";
 
 export default function DashboardPage() {
   const { user } = useUserfront();
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+    <div className="space-y-4 md:space-y-8">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
       <p>Hello, {user.email}</p>
       <h2>Welcome to the Dashboard</h2>
-      <p>This is a protected page. You can see this because you are logged in.</p>
-      <div>
-        <p>Your JWT user:</p>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
+      <p>
+        This is a protected page. You can see this because you are logged in.
+      </p>
+      <div className="mx-auto mt-24">
+        <div className="bg-gray-900 text-white p-4 rounded-md">
+          <div className="flex justify-between items-center mb-2">
+            <span className="text-gray-400">Your JWT user</span>
+          </div>
+          <div className="overflow-x-auto">
+            <pre id="code" className="text-gray-300">
+              <code>{JSON.stringify(user, null, 2)}</code>
+            </pre>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="flex flex-col size-full max-w-5xl p-4 md:p-8 mx-auto min-h-screen">
+        <div className="flex flex-col size-full max-w-5xl p-4 md:p-8 mx-auto min-h-screen gap-y-4 md:gap-y-8">
           <UserfrontProvider
             tenantId={process.env.NEXT_PUBLIC_USERFRONT_WORKSPACE_ID}
             loginRedirect="/dashboard"
